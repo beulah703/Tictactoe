ad777f2ea14c54fb9723566b2d173c835be302f8
beulah703 <68689616+beulah703@users.noreply.github.com>
2023-05-23 12:42:20 +0530

    Update player.py

--- player.py
+++ player.py
@@ -1,4 +1,134 @@
-"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tour Registration</title>
+    <style>
+        .passenger {
+            margin-bottom: 10px;
+        }
+    </style>
+    <script>
+        function calculateTotalFare() {
+            var farePerPerson = 900;
+            var passengerCount = document.getElementsByClassName('passenger').length;
+            var totalFare = farePerPerson * passengerCount;
+            document.getElementById('total-fare').innerText = totalFare;
+        }
+
+        function addPassenger() {
+            var passengerContainer = document.getElementById('passenger-container');
+
+            var passengerDiv = document.createElement('div');
+            passengerDiv.classList.add('passenger');
+
+            var passengerIndex = passengerContainer.childElementCount + 1;
+
+            var nameLabel = document.createElement('label');
+            nameLabel.innerText = 'Name:';
+            var nameInput = document.createElement('input');
+            nameInput.type = 'text';
+            nameInput.name = 'name' + passengerIndex;
+            nameLabel.appendChild(nameInput);
+
+            var emailLabel = document.createElement('label');
+            emailLabel.innerText = 'Email:';
+            var emailInput = document.createElement('input');
+            emailInput.type = 'email';
+            emailInput.name = 'email' + passengerIndex;
+            emailLabel.appendChild(emailInput);
+
+            var mobileLabel = document.createElement('label');
+            mobileLabel.innerText = 'Mobile:';
+            var mobileInput = document.createElement('input');
+            mobileInput.type = 'tel';
+            mobileInput.name = 'mobile' + passengerIndex;
+            mobileLabel.appendChild(mobileInput);
+
+            var ageLabel = document.createElement('label');
+            ageLabel.innerText = 'Age:';
+            var ageInput = document.createElement('input');
+            ageInput.type = 'number';
+            ageInput.name = 'age' + passengerIndex;
+            ageLabel.appendChild(ageInput);
+
+            var genderLabel = document.createElement('label');
+            genderLabel.innerText = 'Gender:';
+            var genderSelect = document.createElement('select');
+            genderSelect.name = 'gender' + passengerIndex;
+            var maleOption = document.createElement('option');
+            maleOption.value = 'male';
+            maleOption.innerText = 'Male';
+            var femaleOption = document.createElement('option');
+            femaleOption.value = 'female';
+            femaleOption.innerText = 'Female';
+            genderSelect.appendChild(maleOption);
+            genderSelect.appendChild(femaleOption);
+            genderLabel.appendChild(genderSelect);
+
+            var deleteButton = document.createElement('button');
+            deleteButton.type = 'button';
+            deleteButton.innerText = '-';
+            deleteButton.onclick = function() {
+                passengerContainer.removeChild(passengerDiv);
+                calculateTotalFare();
+            };
+
+            passengerDiv.appendChild(nameLabel);
+            passengerDiv.appendChild(emailLabel);
+            passengerDiv.appendChild(mobileLabel);
+            passengerDiv.appendChild(ageLabel);
+            passengerDiv.appendChild(genderLabel);
+            passengerDiv.appendChild(deleteButton);
+
+            passengerContainer.appendChild(passengerDiv);
+            calculateTotalFare();
+        }
+
+        function init() {
+            var addButton = document.getElementById('add-passenger');
+            addButton.onclick = addPassenger;
+        }
+
+        window.onload = init;
+    </script>
+</head>
+<body>
+    <h1>Tour Registration</h1>
+    <form>
+        <div id="passenger-container">
+            <div class="passenger">
+                <label for="name1">Name:</label>
+                <input type="text" id="name1" name="name1" required>
+
+                <label for="email1">Email
+                :</label>
+                <input type="email" id="email1" name="email1" required>
+
+                <label for="mobile1">Mobile:</label>
+                <input type="tel" id="mobile1" name="mobile1" required>
+
+                <label for="age1">Age:</label>
+                <input type="number" id="age1" name="age1" required>
+
+                <label for="gender1">Gender:</label>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+            </div>
+        </div>
+        <button type="button" id="add-passenger">+</button>
+
+        <div>
+            <strong>Total Fare:</strong> $<span id="total-fare">900</span>
+        </div>
+
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
+
+
+""
 Tic-Tac-Toe players using inheritance implementation by Kylie YIng
 YouTube Kylie Ying: https://www.youtube.com/ycubed 
 Twitch KylieYing: https://www.twitch.tv/kylieying 
